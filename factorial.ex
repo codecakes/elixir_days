0f807e11be44cb5ea79c5166691685c212cd5be3
codecakes <akurnya@gmail.com>
2016-06-27 15:39:49 +0530

    added debugging  info

--- factorial.ex
+++ factorial.ex
@@ -5,6 +5,21 @@ fact = fn(n) -> Enum.reduce(1..n, 1, fn(num, ac1) -> num * ac1 end) end
 defmodule Factorial do
     @moduledoc """
     Factorial using optimal tail recursion
+    You can use a debugger tracer for debugging:
+    :dbg.tracer()
+    {:ok, #PID<0.74.0>}
+    iex(8)> :dbg.p(:all, :c)
+    {:ok, [{:matched, :nonode@nohost, 44}]}
+    iex(9)> :dbg.tpl(Factorial, :fact, [])
+    {:ok, [{:matched, :nonode@nohost, 2}]}
+    iex(10)> fact 5
+    (<0.58.0>) call 'Elixir.Factorial':fact(5)
+    (<0.58.0>) call 'Elixir.Factorial':fact(1,1,5)
+    (<0.58.0>) call 'Elixir.Factorial':fact(2,2,5)
+    (<0.58.0>) call 'Elixir.Factorial':fact(3,6,5)
+    (<0.58.0>) call 'Elixir.Factorial':fact(4,24,5)
+    (<0.58.0>) call 'Elixir.Factorial':fact(5,120,5)
+
     """
     def fact(x) do
         fact(1, 1, x)
