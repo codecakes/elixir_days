c896ab62fc3ff61cdf9ae4de133c3c6b2308037c
codecakes <akurnya@gmail.com>
2017-06-15 18:25:16 +0530

    test(stackdelivery): tests pass

--- stackdelivery/lib/stackdelivery.ex
+++ stackdelivery/lib/stackdelivery.ex
@@ -1,10 +1,22 @@
 defmodule Stackdelivery do
   @moduledoc """
   Main Stackdelivery entry point.
+  This app starts a bunch of stacks as building blocks and can be joined any which way transferring ejectile between two stacks.
   """
   alias Stackdelivery.StackGen.Supervisor, as: StackSup
   alias Stackdelivery.Stack
 
-  def shoot(stack_name), do: StackSup.shoot(stack_name)
+  @db %{}
+
+  def new(stack_name) do
+    case shoot(stack_name) do
+      {:ok, pid} -> Map.put(@db, stack_name, pid)
+      {:error, _} ->
+        IO.puts "stack name already exists"
+        :not_allowed
+    end
+  end
+
+  defp shoot(stack_name), do: StackSup.shoot(stack_name)
 
 end
--- stackdelivery/test/stackdelivery_test.exs
+++ stackdelivery/test/stackdelivery_test.exs
@@ -1,8 +1,20 @@
 defmodule StackdeliveryTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Stackdelivery
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    :ok  
   end
+
+  test "creates a stack" do
+    assert is_map Stackdelivery.new("stack1")
+  end
+
+  test "creates two different stack pids" do
+    %{"stack2"=> pid2} = Stackdelivery.new("stack2")
+    %{"stack3"=> pid3} = Stackdelivery.new("stack3")
+    
+    refute pid3 == pid2
+  end
+
 end
